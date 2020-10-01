ebff2f4e2920325f50fbeca8dfbdc778922bc5aa
Janar <zhakubai@gmail.com>
2020-10-01 21:44:16 +0600

    inital commit

---
+++ main/link/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class Links(models.Model):
+    old_link = models.URLField(max_length=500)
+    new_link = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
